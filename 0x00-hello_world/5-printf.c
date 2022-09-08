c51afc0e520bbf9227331d3904d76ab80ae937c6
gitDemeke <demeke.asres@aau.edu.et>
2022-09-08 08:41:25 +0300

    Hello World!

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *Return: Always 0 (Success
+**/
+int main(void)
+{
+	char tmp[] = "with proper grammar, but the outcome is a piece of art,";
+
+	printf("%s", tmp);
+	printf("\n");
+	return (0);
+}
